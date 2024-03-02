be39dc190e74907df2035530e5ac9cc2e276d602
polikarmike <polikarpov.mike@yandex.ru>
2024-03-03 01:48:16 +0400

    Fix #5 HW2

--- scrapper/src/main/java/edu/java/utils/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/utils/LinkUpdaterScheduler.java
@@ -2,7 +2,6 @@ package edu.java.utils;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
